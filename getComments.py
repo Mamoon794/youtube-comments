0ddb5022d367e352f950d4856dd8a919d0a8b26e
Mamoon Akhtar <primus@Optimus-Primes-Computer.local>
2023-06-10 20:17:47 -0400

    created youtube requests

--- getComments.py
+++ getComments.py
@@ -3,3 +3,20 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+
+api = os.getenv("API_KEY")
+video_id = os.getenv("VIDEO_ID")
+
+
+youtube = build("youtube", "v3", developerKey=api)
+
+
+request = youtube.commentThreads().list(
+    part="snippet",
+    videoId=video_id,
+    maxResults = 10,
+    order="time"
+
+)
+response = request.execute()
+nextOne = "12"
