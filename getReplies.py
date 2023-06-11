0f4108910686297d7e9903b679df090993a82ca9
Mamoon Akhtar <primus@Optimus-Primes-Computer.local>
2023-06-10 20:37:53 -0400

    added comments to getReplies

--- getReplies.py
+++ getReplies.py
@@ -3,13 +3,15 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
+#The id's for the google api and the youtube id
 api = os.getenv("API_KEY")
 video_id = os.getenv("VIDEO_ID")
 
-
+#Builds the youtube video and gets it ready for extraction
 youtube = build("youtube", "v3", developerKey=api)
 
-
+# requesting the comments 10 at a time in order of when they were commented
 request = youtube.commentThreads().list(
     part="replies",
     videoId=video_id,
